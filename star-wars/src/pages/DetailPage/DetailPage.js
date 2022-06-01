e217acadb9503b963585529fe076a725a89a9b6f
RaniSales <Ranievly5@hotmail.com>
2022-06-01 15:59:24 -0300

    add-routes

---
+++ star-wars/src/pages/DetailPage/DetailPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DetailPage = () =>{
+    return(
+        <div>
+            <h1>Detalhe</h1>
+        </div>
+    )
+}
+
+export default DetailPage;
---
+++ star-wars/src/pages/ListPage/ListPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListPage = () =>{
+    return(
+        <div>
+            <h1>Lista</h1>
+        </div>
+    )
+}
+
+export default ListPage;
